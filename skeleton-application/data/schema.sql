46b2075507982a8d9f73c4cd201b7646d4bebeb9
Onesimo Batista <onesimobatista@gmail.com>
2016-11-02 19:01:20 -0200

    db sqlite connections

---
+++ skeleton-application/data/schema.sql
@@ -0,0 +1,6 @@
+create table post (id INTEGER PRIMARY KEY AUTOINCREMENT), title varchar(255) not null, content text not null);
+
+insert into post(title,content) values ('Post 1', 'content 1');
+insert into post(title,content) values ('Post 2', 'content 2');
+insert into post(title,content) values ('Post 3', 'content 3');
+insert into post(title,content) values ('Post 4', 'content 4');
