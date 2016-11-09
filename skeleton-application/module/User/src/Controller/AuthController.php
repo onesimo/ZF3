954f0e64d3ddcbd07f687b5b6f388e7c1f6b1c51
Onesimo Batista <onesimobatista@gmail.com>
2016-11-09 20:45:01 -0200

    add zend authenttication

--- skeleton-application/module/User/src/Controller/AuthController.php
+++ skeleton-application/module/User/src/Controller/AuthController.php
@@ -24,7 +24,7 @@ class AuthController extends AbstractActionController
     public function loginAction()
     {
         if ($this->authService->hasIdentity()) {
-            return $this->redirect()->toRoute('admin-blog/post');
+           return $this->redirect()->toRoute('post');
         }
         $form = new LoginForm();
         $messageError = null;
@@ -38,8 +38,8 @@ class AuthController extends AbstractActionController
                 $authAdapter->setIdentity($formData['username']);
                 $authAdapter->setCredential($formData['password']);
                 $result = $this->authService->authenticate();
-                if ($result->isValid()) {
-                    return $this->redirect()->toRoute('admin-blog/post');
+                if ($result->isValid()) { 
+                    return $this->redirect()->toRoute('post');
                 } else {
                     $messageError = "Login Inválido!";
                 }
