773610b9fd10282f7d5404908bc008c3838bf9dd
Onesimo Batista <onesimobatista@gmail.com>
2016-11-08 23:02:23 -0200

    create login

--- skeleton-application/module/User/src/Controller/Factory/AuthControllerFactory.php
+++ skeleton-application/module/User/src/Controller/Factory/AuthControllerFactory.php
@@ -1,10 +1,13 @@
 <?php
 namespace User\Controller\Factory;
+
 use Interop\Container\ContainerInterface;
 use User\Controller\AuthController;
 use Zend\Authentication\AuthenticationServiceInterface;
+
 class AuthControllerFactory
 {
+
     public function __invoke(ContainerInterface $container)
     {
         $authService = $container->get(AuthenticationServiceInterface::class);
